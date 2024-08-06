6b839fb3ec1764e4a14a73b7338bf87daba3654c
Abdulholiq13 <abdulholiq571@gmail.com>
2024-08-06 23:46:25 +0500

    updated

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,29 +2,65 @@ import React from 'react';
 
 const Main = () => {
 	const [data, setData] = React.useState([]);
-	const [value, setValue] = React.useState('');
+	const [title, setTitle] = React.useState('');
+	const [price, setPrice] = React.useState('');
+	const [desc, setDesc] = React.useState('');
+
+	const handleCreate = (e) => {
+		e.preventDefault();
+		let users = {
+			id: new Date().getTime(),
+			title,
+			price,
+			desc,
+		};
+		setData((prev) => [...prev, users]);
+		setTitle('');
+		setPrice('');
+		setDesc('');
+	};
+
 	return (
 		<div className='container mt-10 flex items-center justify-center flex-col gap-10'>
-			<form className='max-w-sm mx-auto flex items-center gap-4 justify-center'>
+			<form className='max-w-sm mx-auto flex flex-col gap-4 ' onSubmit={handleCreate}>
 				<div className='mb-5'>
 					<label htmlFor='Name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-						Your name
+						Your product
 					</label>
 					<input
-						type='name'
-						id='name'
+						type='text'
+						id='text'
 						className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-						value={value}
+						defaultValue={title}
+						onChange={(event) => setTitle(event.target.value)}
 						required
 					/>
 				</div>
 				<div className='mb-5'>
-					<label htmlFor='age' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
-						Your age
+					<label
+						htmlFor='price'
+						className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+					>
+						Price
 					</label>
 					<input
-						type='age'
-						id='age'
+						type='price'
+						id='price'
+						defaultValue={price}
+						onChange={(event) => setPrice(event.target.value)}
+						className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+						required
+					/>
+				</div>
+				<div className='mb-5'>
+					<label htmlFor='desc' className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
+						Description
+					</label>
+					<input
+						type='desc'
+						id='desc'
+						defaultValue={desc}
+						onChange={(event) => setDesc(event.target.value)}
 						className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
 						required
 					/>
@@ -37,17 +73,19 @@ const Main = () => {
 				</button>
 			</form>
 			<div className='flex gap-4 justify-start w-full'>
-				<div className='card bg-base-100 w-96 shadow-xl flex items-start justify-center flex-col'>
-					<figure className='w-full h-[226px] bg-[#f4f4f4]'></figure>
-					<div className='card-body'>
-						<h2 className='card-title'>Shoes!</h2>
-						<p>Age is </p>
-						<div className='card-actions justify-end'>
-							<div className='badge badge-outline'>Admin</div>
-							<div className='badge badge-outline'>Id:</div>
+				{data?.map((card) => (
+					<div
+						key={card.id}
+						className='card bg-base-100 w-96 shadow-xl flex items-start justify-center flex-col'
+					>
+						<figure className='w-full h-[226px] bg-[#f4f4f4]'></figure>
+						<div className='card-body'>
+							<h2 className='card-title'>{card.title}</h2>
+							<p>{card.price}</p>
+							<p>{card.desc}</p>
 						</div>
 					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	);
